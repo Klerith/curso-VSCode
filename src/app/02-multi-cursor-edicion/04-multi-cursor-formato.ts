2f3ad1b3532bd2bb5e39aba7768f46b30d63b485
duvan1026 <duvancastro1026@gmail.com>
2022-11-29 15:14:18 -0500

    Implementación #3  shortcut para la creación y manejo del multicursor

--- src/app/02-multi-cursor-edicion/04-multi-cursor-formato.ts
+++ src/app/02-multi-cursor-edicion/04-multi-cursor-formato.ts
@@ -8,10 +8,10 @@
         Ctrl + Alt+ ↑ / ↓
 */
 
-const hulk = 'brouce banner';
-const Hawkeye = 'cinton francis';
-const ironman = 'tony stark';
-const spiderman = 'peter parker';
+const hulk       = 'brouce banner';
+const Hawkeye    = 'cinton francis';
+const ironman    = 'tony stark';
+const spiderman  = 'peter parker';
 const viudaNegra = 'natalia romanova';
 
 
